1dec23ba3e0e6a4941ca5adbdaedb969227458da
diewland <diewland@gmail.com>
2019-02-04 16:06:47 +0700

    add webcam case

--- app.py
+++ app.py
@@ -1,11 +1,16 @@
 from flask import Flask, render_template, request, jsonify
+import base64
 
 app = Flask(__name__)
 
-# index
+# front
 @app.route('/')
 def hello():
     return render_template('index.html')
+
+@app.route('/webcam')
+def webcam():
+    return render_template('webcam.html')
 
 # process
 def process(input_val):
@@ -21,6 +26,19 @@ def trible():
     return jsonify({
         'success': True,
         'data': output_val,
+    })
+
+@app.route('/save_photo', methods=['POST'])
+def save_photo():
+    b64img = request.json.get('img')
+    b64img = b64img.split(',')[1] # remove "data:image/jpeg;base64,"
+
+    with open("./static/latest.jpg", "wb") as fh:
+        fh.write(base64.decodebytes(b64img.encode()))
+
+    return jsonify({
+        'success': True,
+        'message': 'done',
     })
 
 if __name__ == '__main__':
